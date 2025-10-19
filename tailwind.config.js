6fa5bbcdd30a60edb5e072823d765b424c08a6f3
Danylo Mikhnytskyi <@danilmickhnitskyy>
2025-10-19 14:51:23 +0200

    Add types for tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
+  theme: { extend: {} },
   plugins: [],
 };
